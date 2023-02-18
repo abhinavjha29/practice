5543252d3486a7526816c880fa0c925ed4f0d4d6
abhinavjha29 <abhinav_29@outlook.com>
2023-02-18 18:13:20 +0530

    third commit

---
+++ dom.js
@@ -0,0 +1,23 @@
+
+console.dir(document) ;
+console.log(document.domain) ;
+// console.dir (document);
+// console.log(document.domain);
+// console.log(document. URL);
+// console.log(document.title);
+// //document.title = 123;
+// console.log(document.doctype);
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.all);
+// console.log(document.all [10]);
+// // document.all [10].textContent
+// console.log(document.forms [0]);
+let header = document.getElementById("main-header") ;
+console.log(header) ;
+let head = document.getElementById("header-title") ;
+console.log(head) ;
+header.style.borderBottom = "solid 10px #000" ;
+let change = document.getElementById("tit") ;
+change.style.fontWeight= "bold" ;
+change.style.color ="#008000"
